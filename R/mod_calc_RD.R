3989d6975244f65a72b3b37ead1526b7471233d0
Koohyun <bassdesire@gmail.com>
2020-07-20 15:05:18 -0400

    added modsol_RD; checked examples

--- R/mod_calc_RD.R
+++ R/mod_calc_RD.R
@@ -139,3 +139,67 @@ invmod_RD <- function(b, C_pair, Xt, Xc, mon_ind, sigma_t, sigma_c, swap = FALSE
   return(res)
 
 }
+
+#' Modulus of Continuity for the RD Parameter
+#'
+#' Calculates the modulus of continuity for the RD parameter.
+#'
+#' @param delta a nonnegative value of \eqn{\delta}.
+#' @param C_pair a pair of smoothness parameters \eqn{(C, C')}.
+#' @param Xt \eqn{n_t} by \eqn{k} design matrix for the treated units.
+#' @param Xc \eqn{n_c} by \eqn{k} design matrix for the control units.
+#' @param mon_ind index number for monotone variables.
+#' @param sigma_t standard deviation of the error term for the treated units
+#' (either length 1 or \eqn{n_t}).
+#' @param sigma_c standard deviation of the error term for the control units
+#' (either length 1 or \eqn{n_c}).
+#' @param swap indicator for whether we take (C', C) instead of (C, C').
+#'
+#' @return A list of four components. \code{bt} gives the modulus value
+#' corresponding to the treated observations, while \code{delta_t} gives
+#' the value of the inverse modulus corresponding to the treated observations.
+#' \code{bc} and \code{delta_c} give the analogous values for the control observations.
+#' @export
+#'
+#' @examples n <- 500
+#' d <- 2
+#' X <- matrix(rnorm(n * d), nrow = n, ncol = d)
+#' tind <- X[, 1] > 0 & X[, 2] > 0
+#' Xt <- X[tind == 1, ,drop = FALSE]
+#' Xc <- X[tind == 0, ,drop = FALSE]
+#' C_pair <- c(0.1, 0.2)
+#' mon_ind <- c(1, 2)
+#' sigma <- rnorm(n)^2 + 1
+#' sigma_t <- sigma[tind == 1]
+#' sigma_c <- sigma[tind == 0]
+#' modsol_RD(1, C_pair, Xt, Xc, mon_ind, sigma_t, sigma_c, swap = FALSE)
+modsol_RD <- function(delta, C_pair, Xt, Xc, mon_ind, sigma_t, sigma_c, swap = FALSE){
+
+  if (swap) {
+    C_pair <- C_pair[2:1]
+  }
+
+  maxint <- 100 # An arbitrary large number; doesn't affect the result
+
+  minbt <- minb_fun(C_pair, Xt, mon_ind)
+  minbc <- minb_fun(C_pair, Xc, mon_ind, swap = TRUE)
+  minb <- minbt + minbc
+
+  eqn_fun <- function(b) {
+
+    invmod_RD_res <- invmod_RD(b, C_pair, Xt, Xc, mon_ind, sigma_t, sigma_c)
+    delta_t <- invmod_RD_res$delta_t
+    delta_c <- invmod_RD_res$delta_c
+
+    res <- sqrt(delta_t^2 + delta_c^2) - delta
+    return(res)
+  }
+
+  solve <- stats::uniroot(eqn_fun, c(minb, maxint), extendInt = "upX",
+                          tol = .Machine$double.eps^10)
+  bsol <- solve$root
+
+  res <- invmod_RD(bsol, C_pair, Xt, Xc, mon_ind, sigma_t, sigma_c)
+
+  return(res)
+}
